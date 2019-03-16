4fa026d50f11c51152741809b73d1222e94a845d
simoon <420526391@qq.com>
2019-03-16 19:43:50 +0800

    init webpack

---
+++ src/page/common/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @LastEditors: Please set LastEditors
+ * @Date: 2019-03-16 16:17:32
+ * @LastEditTime: 2019-03-16 16:17:57
+ */
+'user strict'
+
+console.log('is global');
---
+++ src/page/index/index.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: In User Settings Edit
+ * @Author: your name
+ * @LastEditors: Please set LastEditors
+ * @Date: 2019-03-16 15:21:03
+ * @LastEditTime: 2019-03-16 16:22:08
+ */
+'use strict';
+require('./index.css');
+require('../module.js');
